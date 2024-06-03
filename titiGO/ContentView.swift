419f8e3d28afced411e260b4c34db14e29e712cf
kkudotomato <70147720+haonanT@users.noreply.github.com>
2024-06-03 10:51:10 +0800

    Initial Commit

---
+++ titiGO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  titiGO
+//
+//  Created by haonan on 2024/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ titiGO/titiGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  titiGOApp.swift
+//  titiGO
+//
+//  Created by haonan on 2024/6/3.
+//
+
+import SwiftUI
+
+@main
+struct titiGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
